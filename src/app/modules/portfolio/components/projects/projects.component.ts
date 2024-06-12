aabfa8009005771d9681a738ad57164a6e99329c
Alex <alex.sandro.1199@gmail.com>
2024-06-12 20:10:10 -0300

    Dialog component

--- src/app/modules/portfolio/components/projects/projects.component.ts
+++ src/app/modules/portfolio/components/projects/projects.component.ts
@@ -1,14 +1,21 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { IProjects } from '../../interface/IProjects.interface';
+
+//Material
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { EDialogPanelClass } from '../../enum/EDialogPanelClass.enum';
+import { DialogProjectsComponent } from '../dialog/dialog-projects/dialog-projects.component';
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [],
+  imports: [MatDialogModule],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.scss',
 })
 export class ProjectsComponent {
+  #dialog = inject(MatDialog);
+
   public arrayProjects = signal<IProjects[]>([
     {
       src: 'assets/img/projects/vfull.png',
@@ -25,65 +32,12 @@ export class ProjectsComponent {
         },
       ],
     },
-    {
-      src: 'assets/img/projects/vfull.png',
-      alt: 'Projeto Vida FullStack',
-      title: 'Vida Fullstack',
-      width: '100px',
-      height: '51px',
-      descripition:
-        '<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cupiditate minus eos a excepturi hic tempora corrupti, laboriosam necessitatibus ea consequuntur nulla quis numquam beatae ipsa, ducimus nesciunt repudiandae maxime recusandae!</p>',
-      links: [
-        {
-          name: 'Conheça o Blog',
-          href: 'https://vidafullstack.com.br',
-        },
-      ],
-    },
-    {
-      src: 'assets/img/projects/vfull.png',
-      alt: 'Projeto Vida FullStack',
-      title: 'Vida Fullstack',
-      width: '100px',
-      height: '51px',
-      descripition:
-        '<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cupiditate minus eos a excepturi hic tempora corrupti, laboriosam necessitatibus ea consequuntur nulla quis numquam beatae ipsa, ducimus nesciunt repudiandae maxime recusandae!</p>',
-      links: [
-        {
-          name: 'Conheça o Blog',
-          href: 'https://vidafullstack.com.br',
-        },
-      ],
-    },
-    {
-      src: 'assets/img/projects/vfull.png',
-      alt: 'Projeto Vida FullStack',
-      title: 'Vida Fullstack',
-      width: '100px',
-      height: '51px',
-      descripition:
-        '<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cupiditate minus eos a excepturi hic tempora corrupti, laboriosam necessitatibus ea consequuntur nulla quis numquam beatae ipsa, ducimus nesciunt repudiandae maxime recusandae!</p>',
-      links: [
-        {
-          name: 'Conheça o Blog',
-          href: 'https://vidafullstack.com.br',
-        },
-      ],
-    },
-    {
-      src: 'assets/img/projects/vfull.png',
-      alt: 'Projeto Vida FullStack',
-      title: 'Vida Fullstack',
-      width: '100px',
-      height: '51px',
-      descripition:
-        '<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cupiditate minus eos a excepturi hic tempora corrupti, laboriosam necessitatibus ea consequuntur nulla quis numquam beatae ipsa, ducimus nesciunt repudiandae maxime recusandae!</p>',
-      links: [
-        {
-          name: 'Conheça o Blog',
-          href: 'https://vidafullstack.com.br',
-        },
-      ],
-    },
   ]);
+
+  public openDialog(data: IProjects) {
+    this.#dialog.open(DialogProjectsComponent, {
+      data,
+      panelClass: EDialogPanelClass.PROJECTS,
+    });
+  }
 }
