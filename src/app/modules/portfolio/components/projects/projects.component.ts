9bd81435c24ce42ad25b3fb4647470bbcb422bf4
Alex <alex.sandro.1199@gmail.com>
2024-06-12 19:41:16 -0300

    projects components

--- src/app/modules/portfolio/components/projects/projects.component.ts
+++ src/app/modules/portfolio/components/projects/projects.component.ts
@@ -25,5 +25,65 @@ export class ProjectsComponent {
         },
       ],
     },
+    {
+      src: 'assets/img/projects/vfull.png',
+      alt: 'Projeto Vida FullStack',
+      title: 'Vida Fullstack',
+      width: '100px',
+      height: '51px',
+      descripition:
+        '<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cupiditate minus eos a excepturi hic tempora corrupti, laboriosam necessitatibus ea consequuntur nulla quis numquam beatae ipsa, ducimus nesciunt repudiandae maxime recusandae!</p>',
+      links: [
+        {
+          name: 'Conheça o Blog',
+          href: 'https://vidafullstack.com.br',
+        },
+      ],
+    },
+    {
+      src: 'assets/img/projects/vfull.png',
+      alt: 'Projeto Vida FullStack',
+      title: 'Vida Fullstack',
+      width: '100px',
+      height: '51px',
+      descripition:
+        '<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cupiditate minus eos a excepturi hic tempora corrupti, laboriosam necessitatibus ea consequuntur nulla quis numquam beatae ipsa, ducimus nesciunt repudiandae maxime recusandae!</p>',
+      links: [
+        {
+          name: 'Conheça o Blog',
+          href: 'https://vidafullstack.com.br',
+        },
+      ],
+    },
+    {
+      src: 'assets/img/projects/vfull.png',
+      alt: 'Projeto Vida FullStack',
+      title: 'Vida Fullstack',
+      width: '100px',
+      height: '51px',
+      descripition:
+        '<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cupiditate minus eos a excepturi hic tempora corrupti, laboriosam necessitatibus ea consequuntur nulla quis numquam beatae ipsa, ducimus nesciunt repudiandae maxime recusandae!</p>',
+      links: [
+        {
+          name: 'Conheça o Blog',
+          href: 'https://vidafullstack.com.br',
+        },
+      ],
+    },
+    {
+      src: 'assets/img/projects/vfull.png',
+      alt: 'Projeto Vida FullStack',
+      title: 'Vida Fullstack',
+      width: '100px',
+      height: '51px',
+      descripition:
+        '<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cupiditate minus eos a excepturi hic tempora corrupti, laboriosam necessitatibus ea consequuntur nulla quis numquam beatae ipsa, ducimus nesciunt repudiandae maxime recusandae!</p>',
+      links: [
+        {
+          name: 'Conheça o Blog',
+          href: 'https://vidafullstack.com.br',
+        },
+      ],
+    },
   ]);
 }
